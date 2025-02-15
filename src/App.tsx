fefe72cb18babe5565048b60c36630d6485df1ba
Calpo <calpo22@gmail.com>
2025-02-15 08:03:39 +0000

    custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,37 @@ import React from "react";
 import "./App.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { UserProvider } from "./contexts/UserContext";
+import useWindowSize from "./hooks/useWindowSize";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 interface Props {
   children: React.ReactNode;
 }
 
 function App({ children }: Props) {
+  const windowSize = useWindowSize();
+  const [permanentName, setPermanentName] = useLocalStorage<string>(
+    "eternalName",
+    "永遠のしんや"
+  );
+
   return (
     <>
       <UserProvider>
         <ThemeProvider>
           <div>{children}</div>
+          <div>
+            window size: {windowSize.width} x {windowSize.height}
+          </div>
+
+          <div>
+            <input
+              type="text"
+              value={permanentName}
+              onChange={(e) => setPermanentName(e.target.value)}
+            />
+            <p>おかえりなさい、{permanentName}さん</p>
+          </div>
         </ThemeProvider>
       </UserProvider>
     </>
---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
+    const [storedValue, setStoredValue] = useState<T>(() => {
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            console.log(error);
+            return initialValue;
+        }
+
+    });
+
+    useEffect(() => {
+        try {
+            window.localStorage.setItem(key, JSON.stringify(storedValue));
+        } catch (error) {
+            console.log(error);
+        }
+    }, [key, storedValue]);
+
+    const setValue = (value: T) => {
+        setStoredValue(value);
+    };
+
+    return [storedValue, setValue];
+}
+
+export default useLocalStorage;
