e1bcdab5edea0a14b709cb523308d607457df058
skanno <1581716+shinyak@users.noreply.github.com>
2025-02-16 15:38:34 +0900

    refactor to route

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import About from "./pages/About";
 import User from "./pages/User";
 import RedirectPage from "./pages/RedirectPage";
 import NotFound from "./pages/NotFound";
+import Greeting from "./components/Greeting";
+import MyForm from "./components/MyForm";
+import ZustandSample from "./components/ZustandSample";
+import WindowSize from "./components/WindowSize";
+import PermanentUserName from "./components/PermanentUserName";
 
 interface Props {
   children: React.ReactNode;
@@ -15,37 +20,40 @@ interface Props {
 
 function App({ children }: Props) {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/user/1">User 1</Link>
-            </li>
-          </ul>
-        </nav>
+    <UserProvider>
+      <ThemeProvider>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/user/1">User 1</Link></li>
+                <li><Link to="/greeting">Greeting</Link></li>
+                <li><Link to="/my-form">My Form</Link></li>
+                <li><Link to="/zustand">Zustand Sample</Link></li>
+                <li><Link to="/window-size">Window Size</Link></li>
+                <li><Link to="/permanent-username">Permanent Username</Link></li>
+              </ul>
+            </nav>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/user/:id/*" element={<User />} />
-          <Route path="/redirect" element={<RedirectPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-
-        <UserProvider>
-          <ThemeProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/user/:id/*" element={<User />} />
+              <Route path="/greeting" element={<Greeting />} />
+              <Route path="/my-form" element={<MyForm placeholder='Enter your name' />} />
+              <Route path="/zustand" element={<ZustandSample />} />
+              <Route path="/window-size" element={<WindowSize />} />
+              <Route path="/permanent-username" element={<PermanentUserName />} />
+              <Route path="/redirect" element={<RedirectPage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
             <div>{children}</div>
-          </ThemeProvider>
-        </UserProvider>
-      </div>
-    </Router>
+          </div>
+        </Router>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Greeting from './components/Greeting.tsx'
-import MyForm from './components/MyForm.tsx'
-import ZustandSample from './components/ZustandSample.tsx';
-import WindowSize from './components/WindowSize.tsx'
-import PermanentUserName from './components/PermanentUserName.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App>
-      <Greeting />
-      <MyForm placeholder='Enter your name' />
-      <ZustandSample /> 
-      <WindowSize />
-      <PermanentUserName />
+    <App children={undefined}>
     </App>
   </StrictMode>
 );
